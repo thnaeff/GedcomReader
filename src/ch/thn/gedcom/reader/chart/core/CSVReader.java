4102cc7b2c2873493a820f15061da01f62013422
thnaeff <thnaeff@bluewin.ch>
2015-06-07 11:45:20 -0400

    Formatting

--- src/ch/thn/gedcom/reader/chart/core/CSVReader.java
+++ src/ch/thn/gedcom/reader/chart/core/CSVReader.java
@@ -34,24 +34,21 @@ import org.supercsv.prefs.CsvPreference;
 import ch.thn.gedcom.reader.GedcomReaderError;
 import ch.thn.gedcom.reader.GedcomReaderException;
 import ch.thn.gedcom.reader.chart.GedcomChartEnums.Columns;
-import ch.thn.util.collections.MultiKeyHashMap;
 
 /**
  * @author Thomas Naeff (github.com/thnaeff)
  *
  */
 public class CSVReader extends AbstractTableReader {
-	
-	protected MultiKeyHashMap<String, CSVCellProcessor> availableColumns = null;
-	
+
 	private ICsvMapReader mapReader = null;
-	
+
 	private CsvPreference columnSeparator = null;
-	
+
 	private CellProcessor[] processors = null;
-		
+
 	private String[] header = null;
-	
+
 	/**
 	 * 
 	 * 
@@ -59,20 +56,20 @@ public class CSVReader extends AbstractTableReader {
 	 * @param valuePreProcessor
 	 * @param linePostProcessor
 	 */
-	public CSVReader(CsvPreference columnSeparator, 
-			ChartValuePreProcessor valuePreProcessor, 
+	public CSVReader(CsvPreference columnSeparator,
+			ChartValuePreProcessor valuePreProcessor,
 			ChartLinePostProcessor linePostProcessor) {
 		super(valuePreProcessor, linePostProcessor);
 		this.columnSeparator = columnSeparator;
-				
+
 	}
-	
+
 	/**
-	 * Opens the given file and checks if the header and the mandatory 
+	 * Opens the given file and checks if the header and the mandatory
 	 * columns are correct.<br>
 	 * <br>
-	 * When opening a file, make sure all the mappings are already set with 
-	 * {@link #addHeaderMapping(String, String)} since they are needed to verify 
+	 * When opening a file, make sure all the mappings are already set with
+	 * {@link #addHeaderMapping(String, String)} since they are needed to verify
 	 * mandatory columns.
 	 * 
 	 * @param csvFileName
@@ -83,13 +80,13 @@ public class CSVReader extends AbstractTableReader {
 	public void open(String csvFileName) throws IOException, FileNotFoundException {
 		open(new InputStreamReader(new FileInputStream(csvFileName), "UTF-8"));
 	}
-	
+
 	/**
-	 * Read from a file with the given input reader. Also checks if the header and the mandatory 
+	 * Read from a file with the given input reader. Also checks if the header and the mandatory
 	 * columns are correct.<br>
 	 * <br>
-	 * When opening a file, make sure all the mappings are already set with 
-	 * {@link #addHeaderMapping(String, String)} since they are needed to verify 
+	 * When opening a file, make sure all the mappings are already set with
+	 * {@link #addHeaderMapping(String, String)} since they are needed to verify
 	 * mandatory columns.
 	 * 
 	 * @param inputReader
@@ -98,33 +95,33 @@ public class CSVReader extends AbstractTableReader {
 	 */
 	public void open(Reader inputReader) throws IOException {
 		mapReader = new CsvMapReader(inputReader, columnSeparator);
-		
+
 		header = mapReader.getHeader(true);
 		processors = createProcessors(header);
-		
+
 	}
-	
+
 	@Override
 	protected Map<Columns, Object> readLine() throws GedcomReaderException {
 		if (mapReader == null || header == null || processors == null) {
 			throw new GedcomReaderError("File not open and initialized");
 		}
-		
+
 		try {
 			Map<String, Object> read = mapReader.read(header, processors);
 			if (read == null) {
 				//EOF
 				return null;
 			}
-			
+
 			Map<Columns, Object> ret = new HashMap<>();
 			for (Entry<String, Object> entry : read.entrySet()) {
 				try {
 					ret.put(Columns.getEnumForValue(entry.getKey()), entry.getValue());
 				} catch (IllegalArgumentException e) {
-					//Enum constant not found because column label is not in the 
+					//Enum constant not found because column label is not in the
 					//standard columns
-//					System.out.println("Column " + entry.getKey() + " not used because it is not a known column.");
+					//					System.out.println("Column " + entry.getKey() + " not used because it is not a known column.");
 				}
 			}
 			return ret;
@@ -133,13 +130,14 @@ public class CSVReader extends AbstractTableReader {
 			throw new GedcomReaderException(e.getMessage());
 		}
 	}
-	
-	
+
+
 	/**
 	 * 
 	 * 
 	 * @throws FamilyChartConverterException
 	 */
+	@Override
 	public void close() throws GedcomReaderException {
 		try {
 			mapReader.close();
@@ -147,13 +145,13 @@ public class CSVReader extends AbstractTableReader {
 			e.printStackTrace();
 			throw new GedcomReaderException(e.getMessage());
 		}
-		
+
 		mapReader = null;
 		header = null;
 		processors = null;
 	}
-	
-	
+
+
 	/**
 	 * Creates a {@link CSVCellProcessor} for each column
 	 * 
@@ -161,17 +159,17 @@ public class CSVReader extends AbstractTableReader {
 	 * @return
 	 */
 	private CellProcessor[] createProcessors(String[] header) {
-		
+
 		ArrayList<CellProcessor> processors = new ArrayList<CellProcessor>();
-		
-		
+
+
 		for (int i = 0; i < header.length; i++) {
 			processors.add(new CSVCellProcessor(header[i], getValuePreProcessor()));
 		}
-		
+
 		return processors.toArray(new CellProcessor[processors.size()]);
 	}
-	
-	
+
+
 
 }
